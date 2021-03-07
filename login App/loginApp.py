ab6a2dec01c8fed8217ed4b3add018e95a87eee8
anandhu720 <anandhu4310@gmail.com>
2021-03-07 21:04:08 +0530

    login app

--- login App/loginApp.py
+++ login App/loginApp.py
@@ -7,32 +7,32 @@ import os
 # Designing window for registration
 
 
-def register():
-    global register_screen
-    register_screen = Toplevel(main_screen)
-    register_screen.title("Register")
-    register_screen.geometry("300x250")
+# def register():
+#     global register_screen
+#     register_screen = Toplevel(main_screen)
+#     register_screen.title("Register")
+#     register_screen.geometry("300x250")
 
-    global username
-    global password
-    global username_entry
-    global password_entry
-    username = StringVar()
-    password = StringVar()
+#     global username
+#     global password
+#     global username_entry
+#     global password_entry
+#     username = StringVar()
+#     password = StringVar()
 
-    Label(register_screen, text="Please enter details below", bg="blue").pack()
-    Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username * ")
-    username_lable.pack()
-    username_entry = Entry(register_screen, textvariable=username)
-    username_entry.pack()
-    password_lable = Label(register_screen, text="Password * ")
-    password_lable.pack()
-    password_entry = Entry(register_screen, textvariable=password, show='*')
-    password_entry.pack()
-    Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10,
-           height=1, bg="blue", command=register_user).pack()
+#     Label(register_screen, text="Please enter details below", bg="blue").pack()
+#     Label(register_screen, text="").pack()
+#     username_lable = Label(register_screen, text="Username * ")
+#     username_lable.pack()
+#     username_entry = Entry(register_screen, textvariable=username)
+#     username_entry.pack()
+#     password_lable = Label(register_screen, text="Password * ")
+#     password_lable.pack()
+#     password_entry = Entry(register_screen, textvariable=password, show='*')
+#     password_entry.pack()
+#     Label(register_screen, text="").pack()
+#     Button(register_screen, text="Register", width=10,
+#            height=1, bg="blue", command=register_user).pack()
 
 
 # Designing window for login
@@ -69,21 +69,21 @@ def login():
 # Implementing event on register button
 
 
-def register_user():
+# def register_user():
 
-    username_info = username.get()
-    password_info = password.get()
+#     username_info = username.get()
+#     password_info = password.get()
 
-    file = open(username_info, "w")
-    file.write(username_info + "\n")
-    file.write(password_info)
-    file.close()
+#     file = open(username_info, "w")
+#     file.write(username_info + "\n")
+#     file.write(password_info)
+#     file.close()
 
-    username_entry.delete(0, END)
-    password_entry.delete(0, END)
+#     username_entry.delete(0, END)
+#     password_entry.delete(0, END)
 
-    Label(register_screen, text="Registration Success",
-          fg="green", font=("calibri", 11)).pack()
+#     Label(register_screen, text="Registration Success",
+#           fg="green", font=("calibri", 11)).pack()
 
 # Implementing event on login button
 
@@ -170,7 +170,7 @@ def main_account_screen():
     Label(text="").pack()
     Button(text="Login", height="2", width="30", command=login).pack()
     Label(text="").pack()
-    Button(text="Register", height="2", width="30", command=register).pack()
+    # Button(text="Register", height="2", width="30", command=register).pack()
 
     main_screen.mainloop()
 
